e0ad439f447fd62992a85eeb93796337277deca4
Firasat Munir <103716152+firasat-munir@users.noreply.github.com>
2022-08-01 17:06:59 +0500

    task13

---
+++ task13.js
@@ -0,0 +1,4 @@
+date=new Date()
+document.write("Current Date: ",date)
+document.write("<br>Elapsed milliseconds since January 1, 1970: ",date.getTime())
+document.write("<br>Elapsed minutes since January 1, 1970: ",date.getTime()/60000)
